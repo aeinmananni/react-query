4c525e4e2624382f191baade36fcaf0890a32425
aein <mananniaein1234@gmail.com>
2025-07-07 23:56:26 +0330

    add New Chnage

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { Users } from "../pages";
-
+/**
+ *
+ * اگر ما 5 تا کامپوننت هم از این نوع بسازیم یک بار درخواست به سرور برای دریافت دیتا زده میشود
+ * دراین حالتی که برسی شده درصورتی که فلگ وجود داشته باشد دیتا برای ما اکتیو و در صورت نبودن فلگ این اکتیو خواهد شد
+ */
 export default function Home() {
+  const [flag, setFlag] = useState<boolean>(false);
   return (
     <div className="w-full h-full border-4 border-yellow-500 flex flex-col">
-      <Users />
+      <span onClick={() => setFlag((S) => !S)}>DDD</span>
+      {flag && <Users />}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 //ابتدا برای این که بخواهیم به کوئری کش متصل شویم باید کوئری کلاینت را ایجاد کنیم
-
+//درجایی که بخواهیم کوئری کلاینتی رو این اکتیو کنیم میتوانیم کلاینت را به صورت دستی تغییر دهیم
+//مدیریت رفرنس کلاینت
 const client: QueryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchInterval: 50000, // میخواهیم بگوییم بعد از هر 5ثانیه دیتارو اپدیت کن
       staleTime: 5000, // میخواهیم بگوییم تا 5 ثانیه دیتا بروز باشد
+      gcTime: 10000, //بودند به طور کلی از کش حذف شود inAcutve دراینجا میگوییم اگر به مدت 10 ثانیه ایتم ها
     },
   },
 });
